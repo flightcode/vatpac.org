44ede2fd494247460ae6a3495f884eade252e28c
Elliot Parker <elliotccp@protonmail.com>
2019-10-27 15:48:21 +1100

    Add navbar shadow on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,10 @@ export class AppComponent {
     if ((document.documentElement.scrollTop || document.body.scrollTop) > 50) {
       document.getElementById("navbar").classList.remove("bg-transparent");
       document.getElementById("navbar").classList.add("bg-dark");
+      document.getElementById("navbar").classList.add("shadow-sm");
     } else {
       document.getElementById("navbar").classList.remove("bg-dark");
+      document.getElementById("navbar").classList.remove("shadow-sm");
       document.getElementById("navbar").classList.add("bg-transparent");
     }
   }
