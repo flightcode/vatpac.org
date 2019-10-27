25fde10678cb16bc58403d7be65a6adf8284fac2
Elliot Parker <elliotccp@protonmail.com>
2019-10-27 15:40:07 +1100

    Add navbar scroll background transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll() {
+    if (document.documentElement.scrollTop > 50) {
+      document.getElementById("navbar").classList.remove("bg-transparent");
+      document.getElementById("navbar").classList.add("bg-dark");
+    } else {
+      document.getElementById("navbar").classList.remove("bg-dark");
+      document.getElementById("navbar").classList.add("bg-transparent");
+    }
+  }
+}
